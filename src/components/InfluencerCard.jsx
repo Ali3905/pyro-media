ce41d9c0bcc42d8069b67e01a2ce770ce9475281
Ali3905 <muhammadali30905@gmail.com>
2024-12-04 11:37:08 +0500

    allow user to search influencer by a simple prompt

--- src/components/InfluencerCard.jsx
+++ src/components/InfluencerCard.jsx
@@ -1,4 +1,4 @@
-import { ChartColumnIncreasing, Eye, Facebook, Heart, Instagram, ThumbsUp } from 'lucide-react'
+import { ChartColumnIncreasing, Eye, Facebook, Heart, Instagram, ThumbsUp, Youtube } from 'lucide-react'
 import React from 'react'
 
 const InfluencerCard = ({ influencer }) => {
@@ -6,25 +6,27 @@ const InfluencerCard = ({ influencer }) => {
         <div className='bg-white flex justify-between overflow-hidden rounded-md'>
             <img src="/dummyAvatar1.jpg" alt="profile" className='w-[280px] aspect-square rounded-full relative -top-[50px] -left-[50px]' />
             <div className='mr-auto flex flex-col gap-[15px] self-center ml-[50px]'>
-                <p className='font-medium text-[24px] relative -left-[30px]'>{influencer.userName}</p>
+                <p className='font-medium text-[24px] relative -left-[30px]'>{influencer?.name}</p>
                 <span className='flex items-center gap-[6px]'>
                     <p className='mr-[10px]'>Type</p>
-                    {
-                        influencer.type.map((type) => {
+                    <p className='p-1 text-[color:var(--dark-green)] border-2 border-[color:var(--dark-green)]'>{influencer?.categoryYouTube}</p>
+                    <p className='p-1 text-[color:var(--dark-green)] border-2 border-[color:var(--dark-green)]'>{influencer?.categoryInstagram}</p>
+                    {/* {
+                        influencer?.type?.map((type) => {
                             return <p className='p-1 text-[color:var(--dark-green)] border-2 border-[color:var(--dark-green)]'>{type}</p>
                         })
-                    }
+                    } */}
                 </span>
-                <p>Total Followers <span className='font-semibold'>{influencer.totalFollowers}</span></p>
+                <p>Total Followers <span className='font-semibold'>{influencer?.totalFollowers}</span></p>
                 <p>Followers</p>
                 <span className='flex gap-4'>
                     <p className='flex gap-2 bg-[color:var(--dark-green)] px-4 py-2 rounded-md text-white'>
-                        <Facebook />
-                        {influencer.facebookFollowers}
+                        <Youtube />
+                        {influencer?.youtubeData.followers}
                     </p>
                     <p className='flex gap-2 bg-[color:var(--dark-green)] px-4 py-2 rounded-md text-white'>
                         <Instagram />
-                        {influencer.instagramFollowers}
+                        {influencer?.instagramData.followers}
                     </p>
                 </span>
             </div>
@@ -33,15 +35,15 @@ const InfluencerCard = ({ influencer }) => {
                 <Heart className='absolute top-[5%] right-[5%] text-[color:var(--green)]' />
                 <span>
                     <p className='font-medium text-[12px] text-[#86848B] flex items-center gap-2'>Engagement Rate <ThumbsUp size={18} /></p>
-                    <p className='font-semibold text-[20px] text-[#312F39]'>{influencer.engagementRate}</p>
+                    <p className='font-semibold text-[20px] text-[#312F39]'>{influencer?.engagementRate}</p>
                 </span>
                 <span>
                     <p className='font-medium text-[12px] text-[#86848B] flex gap-2 items-center'>Followers growth rate <ChartColumnIncreasing size={18} /></p>
-                    <p className='font-semibold text-[20px] text-[#312F39]'>{influencer.followersGrowthRate}</p>
+                    <p className='font-semibold text-[20px] text-[#312F39]'>{influencer?.followersGrowthRate}</p>
                 </span>
                 <span>
                     <p className='font-medium text-[12px] text-[#86848B] flex gap-2 items-center'>View rate <Eye size={18} /></p>
-                    <p className='font-semibold text-[20px] text-[#312F39]'>{influencer.viewRate}</p>
+                    <p className='font-semibold text-[20px] text-[#312F39]'>{influencer?.viewRate}</p>
                 </span>
             </div>
         </div>
--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,44 +1,75 @@
-import React from 'react'
-import { influencers } from '../../data/data'
+import React, { useState } from 'react'
+// import { influencers } from '../../data/data'
 import InfluencerCard from '../../components/InfluencerCard'
+import axios from 'axios'
 
 const index = () => {
+    const [influencers, setInfluencers] = useState([])
+    const [fetchingState, setFetchingState] = useState("idle")
+
+    const getInfluencers = async (prompt) => {
+        setFetchingState("loading")
+        try {
+            const res = await axios({
+                method: "post",
+                baseURL: "http://localhost:8000",
+                url: "/api/ask",
+                data: {
+                    prompt
+                }
+            })
+            if (res.data.data.length < 1) {
+                setFetchingState("notFound")
+            }else {
+                setInfluencers(res.data.data)
+                setFetchingState("success")
+            }
+            
+        } catch (error) {
+            alert("Could not fetch the influencers")
+            setFetchingState("error")
+            console.log(error);
+        }
+    }
     return (
-        <div className='relative bg-black overflow-hidden'>
+        <div className='relative bg-black overflow-hidden min-h-screen'>
             {/* Content container */}
             <div className='max-w-[1200px] mx-auto pt-[130px] relative z-10'>
-                <Header />
-                <InfluencersList influencers={influencers} />
+                <Header handleSubmit={getInfluencers} fetchingState={fetchingState} />
+                <InfluencersList influencers={influencers} fetchingState={fetchingState} />
             </div>
-            
+
             {/* Background div */}
             <div className='h-[400px] w-[300px] rounded-full bg-[color:var(--green)] blur-[180px] shadow-2xl absolute -right-[100px] top-[20vh]'></div>
         </div>
     )
 }
 
-const Header = () => {
+const Header = ({ handleSubmit, fetchingState }) => {
+    const [prompt, setPrompt] = useState("")
     return (
         <div>
             <h1 className='text-[color:var(--light-green)] font-semibold text-[64px] text-center'>Ai Search</h1>
             <h2 className='text-white text-[40px] font-medium text-center'>Find top right  influencer for your brand </h2>
             <div className='mt-[55px] border-2 border-[color:var(--dark-green)] flex p-2 pl-[40px] rounded-xl'>
-                <input type="text" placeholder="Example: I want to find best influencer fashion industry" className='outline-none bg-transparent border-none flex-grow py-2 text-white' />
-                <button className='bg-white px-6 py-2 rounded-md'>Find</button>
+                <input onChange={(e) => setPrompt(e.target.value)} type="text" placeholder="Example: Find influencers who have followers around 50k and posts videos on Vlogs from delhi" className='outline-none bg-transparent border-none flex-grow py-2 text-white' />
+                <button className='bg-white px-6 py-2 rounded-md' onClick={() => handleSubmit(prompt)}>{fetchingState === "loading" ? "Finding" : "Find"}</button>
             </div>
         </div>
     )
 }
 
-export const InfluencersList = ({ influencers }) => {
+export const InfluencersList = ({ influencers, fetchingState }) => {
     return (
-        <div className='flex flex-col gap-[20px] mt-[150px]'>
-            {
-                influencers.map((influencer) => {
-                    return <InfluencerCard influencer={influencer} />
-                })
-            }
-        </div>
+        <>
+            {fetchingState === "idle" ? <p className='text-white text-center mt-[150px]'>Enter the prompt and hit the find buttong to find influencers</p> : fetchingState === "error" ? <p className='text-white text-center mt-[150px]'>Something went wrong while fetching please try ageain</p> : fetchingState === "loading" ? <p className='text-white text-center mt-[150px]'>Fetching influencers Please wait</p> : <div className='flex flex-col gap-[20px] mt-[150px]'>
+                {
+                    influencers?.map((influencer) => {
+                        return <InfluencerCard influencer={influencer} />
+                    })
+                }
+            </div>}
+        </>
     )
 }
 
